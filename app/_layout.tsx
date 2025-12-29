619fbf698672f8b9f517755ac246a72b1b5150c1
qrzmvp <18775311761@163.com>
2025-12-29 19:07:37 +0800

    feat: Remove Head component and set document title for web platform in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,16 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from '../contexts/AuthContext';
-import Head from 'expo-router/head';
+import { Platform } from 'react-native';
 
 export default function RootLayout() {
+  // 在 web 平台设置页面标题
+  if (Platform.OS === 'web' && typeof document !== 'undefined') {
+    document.title = '将军令';
+  }
+
   return (
     <AuthProvider>
-      <Head>
-        <title>将军令</title>
-        <meta name="description" content="将军令 - 专业的交易信号平台" />
-      </Head>
       <StatusBar style="light" />
       <Stack
         screenOptions={{
